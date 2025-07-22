5f191c18aee15c06d9212afc8f3f7c8a6ec707ba
FraPis03 <f.pisacane9@studenti.unisa.it>
2025-07-22 10:41:57 +0200

    aggiunta coordinate

--- SwiftApp/Model/Tappa.swift
+++ SwiftApp/Model/Tappa.swift
@@ -1,4 +1,5 @@
 import SwiftUI
+import CoreLocation
 
 struct Tappa: Identifiable, Equatable, Codable {
     var id: UUID = UUID()
@@ -7,18 +8,22 @@ struct Tappa: Identifiable, Equatable, Codable {
     var oraArrivo: String
     var maps: String
     var foto: String
+    var latitudine:CLLocationDegrees
+    var longitudine:CLLocationDegrees
     
     enum CodingKeys: String, CodingKey {
-        case id, nome, descr, oraArrivo, maps,foto
+        case id, nome, descr, oraArrivo, maps, foto, latitudine, longitudine
     }
     
-    init(id: UUID = UUID(), nome: String, descr: String, oraArrivo: String, foto: String, maps: String) {
+    init(id: UUID = UUID(), nome: String, descr: String, oraArrivo: String, foto: String, maps: String,latitudine:CLLocationDegrees, longitudine:CLLocationDegrees) {
         self.id = id
         self.nome = nome
         self.descr = descr
         self.oraArrivo = oraArrivo
         self.foto = foto
         self.maps = maps
+        self.latitudine = latitudine
+        self.longitudine = longitudine
     }
     
     init(from decoder: Decoder) throws {
@@ -29,6 +34,8 @@ struct Tappa: Identifiable, Equatable, Codable {
         oraArrivo = try container.decode(String.self, forKey: .oraArrivo)
         maps = try container.decode(String.self, forKey: .maps)
         foto = try container.decode(String.self, forKey: .foto)
+        latitudine=try container.decode(CLLocationDegrees.self, forKey: .latitudine)
+        longitudine=try container.decode(CLLocationDegrees.self, forKey: .longitudine)
     }
     
     func encode(to encoder: Encoder) throws {
@@ -39,5 +46,7 @@ struct Tappa: Identifiable, Equatable, Codable {
         try container.encode(oraArrivo, forKey: .oraArrivo)
         try container.encode(maps, forKey: .maps)
         try container.encode(foto, forKey: .foto)
+        try container.encode(latitudine, forKey: .latitudine)
+        try container.encode(longitudine, forKey: .longitudine)
     }
 } 
